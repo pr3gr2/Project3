29204ec82ee1a2cdee9a7fd18726b5c6b0632d0e
Zohaib Ali <s.zohaibali3@gmail.com>
2021-09-15 20:17:09 -0400

    added some typeDefs and model User Chat and Room

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,7 +1,17 @@
 const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
+type User {
+  _id: ID
+  username: String
+  email: String
+  friendCount: Int
+  friends: [User]
+}
 
+type Query {
+  me: User
+}
 `;
 
 module.exports = typeDefs;
