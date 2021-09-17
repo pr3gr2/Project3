d5f30924a9c71408d43a7a26213d68ad40951a94
NayanSidhu <nsidh068@uottawa.ca>
2021-09-17 14:08:14 -0400

    configuered login page front end

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,41 @@
 import React, { Component }  from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import './App.css';
+import './assets/css/index.css'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  // return (
+    
+  //   <div className="container">
+  //     <div className="row">
+  //       <div className="col-4"></div>
+  //       <div className="col-4" align="center">
+  //         <img className ="Logo img-fluid" src={require(`./assets/images/Logo.png`)} alt="ChatBoxLogo"/>
+  //       </div>
+  //       <div className="col-4"></div>
+  //     </div>
+  //     <div className="row background">
+  //       <div className="col-6">
+  //         <form className="form">
+  //           <h3 className ="formName">Login</h3>
+  //           <div className="mb-3">
+  //             <input placeholder ="Email"type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
+  //             <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
+  //           </div>
+  //           <div className="mb-3">
+  //             <input placeholder ="Password" type="password" className="form-control" id="exampleInputPassword1"/>
+  //           </div>
+  //           <div className="mb-3 form-check">
+  //             <input type="checkbox" className="form-check-input" id="exampleCheck1"/>
+  //             <label className="form-check-label" >Remember Me</label>
+  //           </div>
+  //           <button type="submit" className="buttn">Start Chatting</button>
+  //           <p className="text">Not registered? <a href="#">Sign Up</a></p>
+  //         </form>
+  //       </div>
+  //     </div>
+  //   </div>
+    
+  // );
 }
 
 export default App;
--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './App.css';
+import '../src/assets/css/index.css'; 
 import App from './App';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { ApolloProvider } from '@apollo/client';
+import 'bootstrap/dist/css/bootstrap.css';
 
 ReactDOM.render(
   <React.StrictMode>
