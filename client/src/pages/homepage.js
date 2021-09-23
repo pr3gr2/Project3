2f976e58fd9b07fffc57d6be31e48e4613be7dd1
Efren Francisco Jose Palacios Puma <68364026+PanchoPuma@users.noreply.github.com>
2021-09-22 23:25:38 -0700

    added functions

--- client/src/pages/homepage.js
+++ client/src/pages/homepage.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import '../assets/css/index.css';
 import Auth from '../utils/auth';
-import {Chat} from '../components/chat';
+import { Chat } from '../components/chat';
 import { QUERY_ME, QUERY_ALLUSERS } from '../utils/queries';
 import { Link } from 'react-router-dom';
 import { useQuery } from '@apollo/client';
@@ -21,39 +21,70 @@ function HomePage() {
   const { loading, data } = useQuery(QUERY_ALLUSERS);
   const users = data?.users || [];
   const loggedIn = Auth.loggedIn();
-  
+
   console.log(loggedIn)
   console.log(userData)
   console.log(data)
 
 
-const ChatPage = () =>{
-    return(
-      <Chat/>
+  const ChatPage = () => {
+    return (
+      <Chat />
     )
   }
 
-  return(
+  const allUsers = () => {
+
+    return (
+      <div className="row">
+        <div className=" messagesList">
+          {users.map(user => (
+            <ul className="messagesPreview">
+              <a>
+                <li className="messagesContainer">
+
+                  <Button
+                    type='submit'
+                    id="startChatting"
+                    variant='success'
+                    onSubmit={ChatPage}
+                  >
+                    {capitalizeFirstLetter(user.username)}
+                  </Button>
+                </li>
+              </a>
+            </ul>
+          ))}
+        </div>
+      </div>
+    )
+
+  }
+
+  ///main return code
+
+  return (
     <div className="container mainContainer">
       <div align="center">
-        <img className ="Logo img-fluid" src={require(`../assets/images/Logo.png`)} alt="ChatBoxLogo"/>
+        <img className="Logo img-fluid" src={require(`../assets/images/Logo.png`)} alt="ChatBoxLogo" />
       </div>
       <div className="row">
         <div className="col-3 leftcol">
-          <img className ="userProfile img-fluid" src={require(`../assets/images/userStatic.png`)} alt="ChatBoxLogo"/>
-          <p className="userName">{loggedIn && userData ? <div className="col-12 col-lg-3 mb-3">{capitalizeFirstLetter(userData.me.username)} </div>: null}</p>
-        <div className="row options">
-          {/* <div className="col">
+          <img className="userProfile img-fluid" src={require(`../assets/images/userStatic.png`)} alt="ChatBoxLogo" />
+          <p className="userName">{loggedIn && userData ? <div className="col-12 col-lg-3 mb-3">{capitalizeFirstLetter(userData.me.username)} </div> : null}</p>
+          <div className="row options">
+            {/* <div className="col">
             <a href="#">CHAT</a>
           </div> */}
-          <div className="col">
-            <a href="#">USERS</a>
+            <div className="col">
+              <a href="#">USERS</a>
+            </div>
+            <div className="col">
+              <a href="#">FRIENDS</a>
+            </div>
           </div>
-          <div className="col">
-            <a href="#">FRIENDS</a>
-          </div>
-        </div>
-        <div className="row">
+          {allUsers()}
+          {/* <div className="row">
           <div className=" messagesList">                 
             {users.map(user => (
                 <ul className ="messagesPreview">
@@ -73,34 +104,34 @@ const ChatPage = () =>{
                 </ul>
               ))}
           </div>
+        </div> */}
         </div>
-      </div>
 
-      <div className="col-9 chatname">
-        Chat
-      {/* {users.map(user => user.username)} */}
-        <div className="container messageContainer">
-        {ChatPage()} 
-          <div className="row test2">
-            <div className="col-1 icons">
-              <a href="#">
-                <FontAwesomeIcon icon="smile-beam" />
-              </a>
-            </div>
-            <div className="col-10">
-              <textarea className="form-control messageBox" placeholder="Type your message here" id="floatingTextarea">
-              </textarea>
-            </div>
-            <div className="col-1 icons">
-              <a href="#">
-                <FontAwesomeIcon icon="paper-plane" />
-              </a>
+        <div className="col-9 chatname">
+          Chat
+          {/* {users.map(user => user.username)} */}
+          <div className="container messageContainer">
+            {ChatPage()}
+            <div className="row test2">
+              <div className="col-1 icons">
+                <a href="#">
+                  <FontAwesomeIcon icon="smile-beam" />
+                </a>
+              </div>
+              <div className="col-10">
+                <textarea className="form-control messageBox" placeholder="Type your message here" id="floatingTextarea">
+                </textarea>
+              </div>
+              <div className="col-1 icons">
+                <a href="#">
+                  <FontAwesomeIcon icon="paper-plane" />
+                </a>
+              </div>
             </div>
           </div>
         </div>
       </div>
     </div>
-  </div>
   );
 };
 
